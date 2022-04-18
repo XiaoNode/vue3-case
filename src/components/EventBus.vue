e3dbce1acb7124def57d34e5939f73bf30d897f5
jone.xue <905044086@qq.com>
2022-04-18 18:31:28 +0800

    modify bus component

--- src/components/EventBus.vue
+++ src/components/EventBus.vue
@@ -1,7 +1,19 @@
 <template>
 这是组件：EventBus
-<p>Vue3中去掉自带的bus,可使用第三方event-bus</p>
+<p>Vue3中去掉自带的bus,可使用第三方event-bus等插件</p>
   获取root级function<button @click="getRootMsg">获取</button>
+  <pre style="background:#dcdcdc; width:50%; padding:10px;">
+V2:
+    
+const bus = new Vue()
+
+...
+bus.$emit('updateCount')
+...
+bus.$on('vtab-index', (data) => {
+  ...
+})
+  </pre>
 </template>
 <script lang="ts">
 import { defineComponent, toRefs, onMounted, reactive } from "vue";
